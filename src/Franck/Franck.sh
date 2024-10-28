6d165d4748436ff306a6aff96e47bce415f852c6
Mathis <120995618+shell-don@users.noreply.github.com>
2024-10-28 17:58:58 +0100

    Update Franck.sh

--- src/Franck/Franck.sh
+++ src/Franck/Franck.sh
@@ -2,7 +2,7 @@
 # Un algorithme de chiffrement sous stéroides.
 
 ##########
-# Set Up #
+# Se† Up #
 ##########
 
 KEYFILEPATH=<Key_File_path>
@@ -10,13 +10,13 @@ YUBIKEY=<slot[:serial]>
 DBPATH=<DataBase_path>
 
 ########
-# PATH #
+# Pa†H # 
 ########
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin:/dev:/Applications/KeePassXC.app/contents/MacOS:/opt/homebrew/bin:/opt/homebrew/Cellar/openssl@3/3.3.2/bin: 
 
 ########
-# Code #
+# Co∂e #
 ########
 
 echo "Quel est le chemin du fichier à chiffrer ?"
@@ -27,24 +27,29 @@ echo "Quel est le mot de passe de la database ? "
 read -s -r DBPWRD
 
 FILE=${FILEPATH##*/}
-echo $DBPWRD | keepassxc-cli mkdir -y $YUBIKEY -k $KEYFILEPATH -q $DBPATH $FILE
+FRANCK=("F" "R" "A" "N" "C" "K")
+
+keepassxc-cli mkdir -y $YUBIKEY -k $KEYFILEPATH -q $DBPATH $FILE <<< $DBPWRD
 i=1 
 until [[ $i -gt $N ]]
 	do
-		echo $DBPWRD | keepassxc-cli add -y $YUBIKEY -k $KEYFILEPATH -q -g -L 999 -e -c ѮѰѠѪѦѬѨ $DBPATH $FILE/$i
-		X=$(echo $DBPWRD | keepassxc-cli show -s -a password -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE/$i)
-		openssl enc -aes-256-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
-		dd if=${FILEPATH}.enc of=$FILEPATH status=none
-		rm ${FILEPATH}.enc
-		openssl enc -camellia-256-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
-		dd if=${FILEPATH}.enc of=$FILEPATH status=none
-		rm ${FILEPATH}.enc
-		openssl enc -des-ede3-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
-		dd if=${FILEPATH}.enc of=$FILEPATH status=none
-		rm ${FILEPATH}.enc
-		openssl enc -sm4-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
-		dd if=${FILEPATH}.enc of=$FILEPATH status=none
-		rm ${FILEPATH}.enc
+		keepassxc-cli add -y $YUBIKEY -k $KEYFILEPATH -q -g -L 999 -e -c ѮѰѠѪѦѬѨ $DBPATH $FILE/$i <<< $DBPWRD
+		X=$(keepassxc-cli show -s -a password -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE/$i <<< $DBPWRD)
+		for x in "${FRANCK[@]}" 
+			do 
+				openssl enc -aes-256-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
+				dd if=${FILEPATH}.enc of=$FILEPATH status=none
+				rm ${FILEPATH}.enc
+				openssl enc -camellia-256-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
+				dd if=${FILEPATH}.enc of=$FILEPATH status=none
+				rm ${FILEPATH}.enc
+				openssl enc -des-ede3-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
+				dd if=${FILEPATH}.enc of=$FILEPATH status=none
+				rm ${FILEPATH}.enc
+				openssl enc -sm4-cbc -e -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
+				dd if=${FILEPATH}.enc of=$FILEPATH status=none
+				rm ${FILEPATH}.enc
+			done
 		echo "Chiffrement $i effectué"
 		((i++))
 	done
