94bece90cdbae401cd4ac5715f08748db7a81c58
Mathis <120995618+shell-don@users.noreply.github.com>
2024-10-28 17:36:31 +0100

    Optimisation

--- src/AutoCryptR.sh
+++ src/AutoCryptR.sh
@@ -2,7 +2,7 @@
 # Algorithme d'automatisation (déchiffrement) d'AES-256-CBC via une database kdbx à trois facteurs d'authentification
 
 ##########
-# Set Up #
+# Se† Up #
 ##########
 
 KEYFILEPATH=<Key_File_path>
@@ -10,13 +10,13 @@ YUBIKEY=<slot[:serial]>
 DBPATH=<DataBase_path>
 
 ########
-# PATH #
+# Pa†H # 
 ########
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin:/dev:/Applications/KeePassXC.app/contents/MacOS:/opt/homebrew/bin:/opt/homebrew/Cellar/openssl@3/3.3.2/bin: 
 
 ########
-# Code #
+# Co∂e #
 ########
 
 echo "Quel est le chemin du fichier à déchiffrer ?"
@@ -31,7 +31,7 @@ i=1
 until [[ $N -lt $i ]]
 	do
 		
-		X=$(echo $DBPWRD | keepassxc-cli show -s -a password -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE/$N)
+		X=$(keepassxc-cli show -s -a password -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE/$N <<< $DBPWRD)
 		openssl enc -d -aes-256-cbc -in $FILEPATH -out ${FILEPATH}.enc -salt -pbkdf2 -k $X
 		dd if=${FILEPATH}.enc of=$FILEPATH status=none
 		rm ${FILEPATH}.enc
@@ -39,5 +39,5 @@ until [[ $N -lt $i ]]
 		((N--))
 	done
 
-echo $DBPWRD | keepassxc-cli rmdir -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE
-echo $DBPWRD | keepassxc-cli rmdir -y $YUBIKEY -k $KEYFILEPATH $DBPATH Corbeille
+keepassxc-cli rmdir -y $YUBIKEY -k $KEYFILEPATH $DBPATH $FILE <<< $DBPWRD
+keepassxc-cli rmdir -y $YUBIKEY -k $KEYFILEPATH $DBPATH Corbeille <<< $DBPWRD
